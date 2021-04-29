a60032559c78c46c19217ab31a24996c41a618a5
blazej <blazejplis@gmail.com>
2021-04-30 00:51:30 +0200

    Comments update, added labels in bar chart, progress bar optimization

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
 $(document).ready(function () {
-
-
-  //create global array
+  //create global arrays to gather data to it and make it accesible from every function in code
   btcRateUSD = [];
   btcRateGBP = [];
   btcRateEUR = [];
   btcRateUsdGbpEur = [];
   btcTime = [];
-  //create global flag to start script
-  start = 0;
-  //draw chart
+
+
+
+  //draw line chart function
   function drawChart(arr) {
     if (arr.length > 0) {
       new Chartist.Line('#line-chart', {
         series: [
-          arr
-
+          arr //array with data
         ],
         labels: btcTime
       }, {
         fullWidth: true,
-        chartPadding: {
-          right: 50
-        },
-        showArea: true,
+        showArea: true, //show area under chart line
         plugins: [
-          Chartist.plugins.ctPointLabels({
+          Chartist.plugins.ctPointLabels({ //plugin which shows label above point
             textAnchor: 'middle'
           })
         ]
@@ -34,20 +29,20 @@ $(document).ready(function () {
 
   }
 
+  //bar chart function
   function drawBarChart(arr) {
-    // let chartBar = new Chartist.Bar('.ct-chart', {
-    //     labels: ['GBP', 'USD', 'EUR'],
-    //     series: [100,200,300]
-    //   }, {
-    //     distributeSeries: true
-    //   });
     new Chartist.Bar('#bar-chart', {
       labels: ['GBP £', 'DOLLAR $', 'EURO €'],
-      series: arr
+      series: arr //array with data
     }, {
       distributeSeries: true,
       fullWidth: true,
-      showArea: true
+      showArea: true,
+      plugins: [
+        Chartist.plugins.ctPointLabels({ //plugin which shows label above point
+          textAnchor: 'middle'
+        })
+      ]
     });
 
   }
@@ -59,11 +54,12 @@ $(document).ready(function () {
       dataType: "json",
 
       success: function (data) {
-                //timer
+
+        //timer
         //https://www.growthsnippets.com/30-second-countdown-timer-javascript/
-        var remainingTime = 28;
-        var elem = document.getElementById('countdown_div');
-        var timer = setInterval(countdown, 1000); //set the countdown to every second
+        let remainingTime = 28;
+        let elem = document.getElementById('countdown_div');
+        let timer = setInterval(countdown, 1000); //set the countdown to every second
         function countdown() {
           if (remainingTime == -1) {
             clearTimeout(timer);
@@ -74,22 +70,29 @@ $(document).ready(function () {
         }
         //timer end
 
-        btcRateUSD.push(Math.floor(data["bpi"]["USD"]["rate_float"])); //add newest BTC rate to array, and floor
-        btcRateGBP.push(Math.floor(data["bpi"]["GBP"]["rate_float"])); //add newest BTC rate to array, and floor
-        btcRateEUR.push(Math.floor(data["bpi"]["EUR"]["rate_float"])); //add newest BTC rate to array, and floor
-        btcTime.push(data["time"]["updateduk"].slice(15, -4)); //add time for newest BTC rate
+        btcRateUSD.push(Math.floor(data["bpi"]["USD"]["rate_float"])); //add newest BTC rate in USD to array, and floor it
+        btcRateGBP.push(Math.floor(data["bpi"]["GBP"]["rate_float"])); //add newest BTC rate in GBP to array, and floor it
+        btcRateEUR.push(Math.floor(data["bpi"]["EUR"]["rate_float"])); //add newest BTC rate in EUR to array, and floor it
+        btcTime.push(data["time"]["updateduk"].slice(15, -4)); //add time for newest BTC rate, slice it so it will print time only, without date and timezone
 
+        //get all newest rates to one array
         btcRateUsdGbpEur = [btcRateGBP[btcRateGBP.length - 1], btcRateUSD[btcRateUSD.length - 1], btcRateEUR[btcRateEUR.length - 1]];
-        start++;
         console.log("BTC RATE:" + btcRateUSD);
+
+        //draw bar chart with data from array
         drawBarChart(btcRateUsdGbpEur);
+
+        //draw bar chart with data from array
         drawChart(btcRateUSD);
-        
+
       },
       complete: function () {
         // Schedule the next request when the current one's complete
         setTimeout(getBitcoinValue, 30000);
       }
+      // ajax req end
     });
+    //getBitcoinValue end
   })();
+  // document ready function end
 });
